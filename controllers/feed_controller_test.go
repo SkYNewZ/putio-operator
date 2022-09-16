c090ac5e3b32bc16df20d825e110b59d040809fe
Quentin Lemaire <quentin@lemairepro.fr>
2022-09-16 20:03:03 +0200

    Add integration tests for controller

--- controllers/feed_controller_test.go
+++ controllers/feed_controller_test.go
@@ -2,12 +2,20 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"testing"
+	"time"
 
 	skynewzdevv1alpha1 "github.com/SkYNewZ/putio-operator/api/v1alpha1"
 	"github.com/SkYNewZ/putio-operator/internal/putio"
 	"github.com/google/go-cmp/cmp"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"golang.org/x/oauth2"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 func Test_makePutioFeedFromSpec(t *testing.T) {
@@ -210,6 +218,166 @@ func Test_isAlreadyProcessed(t *testing.T) {
 			}
 		})
 	}
+}
+
+var _ = Describe("Feed controller", func() {
+	// Define utility constants for object names and testing timeouts/durations and intervals.
+	const (
+		FeedName        = "test-feed"
+		FeedNamespace   = "default"
+		SecretName      = "putio-token-test"
+		SecretKeyName   = "token"
+		SecretNamespace = FeedNamespace
+
+		timeout  = time.Second * 10
+		interval = time.Millisecond * 250
+	)
+
+	Context("When creating a feed", func() {
+		It("Should create a Feed", func() {
+			By("By Creating creating the putio secret")
+			putioSecret := &corev1.Secret{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "Secret",
+					APIVersion: "v1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      SecretName,
+					Namespace: SecretNamespace,
+				},
+				Immutable: nil,
+				Data:      nil,
+				StringData: map[string]string{
+					SecretKeyName: os.Getenv("PUTIO_TESTING_OAUTH_TOKEN"),
+				},
+				Type: corev1.SecretTypeOpaque,
+			}
+			Expect(k8sClient.Create(ctx, putioSecret)).Should(Succeed())
+
+			By("By Creating the feed")
+			feed := &skynewzdevv1alpha1.Feed{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "Feed",
+					APIVersion: "putio.skynewz.dev/v1alpha1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      FeedName,
+					Namespace: FeedNamespace,
+				},
+				Spec: skynewzdevv1alpha1.FeedSpec{
+					Title:        FeedName,
+					RssSourceURL: "https://www.google.com",
+					ParentDirID: func() *uint {
+						v := uint(0)
+						return &v
+					}(),
+					DeleteOldFiles:       boolToPtr(false),
+					DontProcessWholeFeed: boolToPtr(false),
+					Keyword:              "foo",
+					UnwantedKeywords:     "",
+					Paused:               boolToPtr(true),
+					AuthSecretRef: skynewzdevv1alpha1.AuthSecretReference{
+						Name: SecretName,
+						Key:  SecretKeyName,
+					},
+				},
+				Status: skynewzdevv1alpha1.FeedStatus{},
+			}
+			Expect(k8sClient.Create(ctx, feed)).Should(Succeed())
+
+			feedLookupKey := types.NamespacedName{Name: FeedName, Namespace: FeedNamespace}
+			createdFeed := &skynewzdevv1alpha1.Feed{}
+
+			// We'll need to retry getting this newly created Feed, given that creation may not immediately happen.
+			Eventually(func() bool {
+				return k8sClient.Get(ctx, feedLookupKey, createdFeed) == nil
+			}, timeout, interval).Should(BeTrue())
+
+			// retry getting for the status become updated
+			By("By checking feed ID in status")
+			Eventually(func() (*uint, error) {
+				if err := k8sClient.Get(ctx, feedLookupKey, createdFeed); err != nil {
+					return nil, err
+				}
+
+				return createdFeed.Status.ID, nil
+			}, timeout, interval).ShouldNot(BeNil())
+
+			By("By checking finalizer")
+			Expect(createdFeed.Annotations[finalizerAnnotation]).ShouldNot(BeNil())
+
+			By("By checking statuses conditions")
+			Expect(createdFeed.Status.Conditions).ShouldNot(BeEmpty())
+
+			By("By checking feed on Put.io")
+			f, err := getPutioFeed(createdFeed.Status.ID)
+			Expect(err).Should(BeNil())
+			Expect(f).ShouldNot(BeNil())
+			Expect(f.Keyword).Should(Equal("foo"))
+
+			By("By checking title")
+			Expect(f.Title).Should(Equal(fmt.Sprintf(titleFormat, createdFeed.Spec.Title, createdFeed.GetGeneration())))
+		})
+	})
+
+	Context("When updating a feed", func() {
+		It("Should update Put.io feed from spec", func() {
+			feedLookupKey := types.NamespacedName{Name: FeedName, Namespace: FeedNamespace}
+			createdFeed := &skynewzdevv1alpha1.Feed{}
+			Eventually(func() bool {
+				return k8sClient.Get(ctx, feedLookupKey, createdFeed) == nil
+			}, timeout, interval).Should(BeTrue())
+
+			createdFeed.Spec.Keyword = "bar"
+			createdFeed.Spec.UnwantedKeywords = "foo"
+			Expect(k8sClient.Update(ctx, createdFeed)).Should(Succeed())
+
+			// wait for update and check
+			Eventually(func() bool {
+				f, err := getPutioFeed(createdFeed.Status.ID)
+				if err != nil {
+					return false
+				}
+
+				if f.Keyword != "bar" {
+					return false
+				}
+
+				if f.UnwantedKeywords != "foo" {
+					return false
+				}
+
+				return true
+			}, timeout, time.Second).Should(BeTrue())
+		})
+	})
+
+	Context("When deleting a feed", func() {
+		It("Should delete the feed", func() {
+			feedLookupKey := types.NamespacedName{Name: FeedName, Namespace: FeedNamespace}
+			createdFeed := &skynewzdevv1alpha1.Feed{}
+			Expect(k8sClient.Get(ctx, feedLookupKey, createdFeed)).Should(Succeed())
+
+			By("By deleting the feed")
+			Expect(k8sClient.Delete(ctx, createdFeed)).Should(Succeed())
+			Eventually(func() error {
+				return k8sClient.Get(ctx, feedLookupKey, createdFeed)
+			}, timeout, interval).ShouldNot(BeNil())
+
+			By("By checking feed is deleted on Put.io")
+			f, err := getPutioFeed(createdFeed.Status.ID)
+			Expect(err).ShouldNot(BeNil())
+			Expect(f).Should(BeNil())
+		})
+	})
+})
+
+func getPutioFeed(feedID *uint) (*putio.Feed, error) {
+	tokenSource := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: os.Getenv("PUTIO_TESTING_OAUTH_TOKEN")})
+	oauthClient := oauth2.NewClient(ctx, tokenSource)
+	putioClient := putio.New(context.Background(), oauthClient)
+
+	return putioClient.Rss.Get(ctx, *feedID)
 }
 
 func boolToPtr(v bool) *bool {
--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -17,6 +17,7 @@ limitations under the License.
 package controllers
 
 import (
+	"context"
 	"path/filepath"
 	"testing"
 
@@ -25,6 +26,7 @@ import (
 	. "github.com/onsi/gomega"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
@@ -40,6 +42,8 @@ var (
 	cfg       *rest.Config
 	k8sClient client.Client
 	testEnv   *envtest.Environment
+	ctx       context.Context
+	cancel    context.CancelFunc
 )
 
 func TestAPIs(t *testing.T) {
@@ -52,6 +56,7 @@ func TestAPIs(t *testing.T) {
 
 var _ = BeforeSuite(func() {
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+	ctx, cancel = context.WithCancel(context.Background())
 
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
@@ -73,9 +78,28 @@ var _ = BeforeSuite(func() {
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
 	Expect(err).NotTo(HaveOccurred())
 	Expect(k8sClient).NotTo(BeNil())
+
+	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme: scheme.Scheme,
+	})
+	Expect(err).ToNot(HaveOccurred())
+
+	err = (&FeedReconciler{
+		Client:   k8sManager.GetClient(),
+		Scheme:   k8sManager.GetScheme(),
+		Recorder: k8sManager.GetEventRecorderFor("test"),
+	}).SetupWithManager(k8sManager)
+	Expect(err).ToNot(HaveOccurred())
+
+	go func() {
+		defer GinkgoRecover()
+		err = k8sManager.Start(ctx)
+		Expect(err).ToNot(HaveOccurred(), "failed to run manager")
+	}()
 }, 60)
 
 var _ = AfterSuite(func() {
+	cancel()
 	By("tearing down the test environment")
 	err := testEnv.Stop()
 	Expect(err).NotTo(HaveOccurred())
